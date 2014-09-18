5ee64276156d5ab92ac2e4461225a6ab993fdb0f
Guillermo Heras <guillermo.heras@gmail.com>
2014-09-18 11:46:09 +0200

    Add adjective list

---
+++ js/controller/ctAspects.js
@@ -0,0 +1,22 @@
+/*
+ Significant personality traits or beliefs (Sucker for a Pretty Face, Never Leave a Man Behind, The Only Good Tsyntavian Is a Dead Tsyntavian).
+ */
+/*
+ The character’s background or profession (Educated at the Academy of Blades, Born a Spacer, Cybernetic Street Thief).
+ */
+/*
+ An important possession or noticeable feature (My Father’s Bloodstained Sword, Dressed to the Nines, Sharp Eyed Veteran).
+ */
+/*
+ Relationships to people and organizations (In League with the Twisting Hand, The King’s Favor, Proud Member of the Company of Lords).
+ */
+/*
+ Problems, goals, or issues the character is dealing with (A Price on My Head, The King Must Die, Fear of Heights).
+ */
+/*
+ Titles, reputations, or obligations the character may have (Self-Important Merchant Guildmaster, Silver-Tongued Scoundrel, Honor-Bound to Avenge My Brother).
+ */
+
+/*
+ "I am an [adjective] [noun] who [verbs]"
+ */
