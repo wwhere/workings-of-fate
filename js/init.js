23a0874148a4d6c98aa9714e7bf4e419c43d3423
Guillermo Heras <guillermo.heras@gmail.com>
2014-12-22 17:59:59 +0100

    REfactor

--- js/init.js
+++ js/init.js
@@ -56,37 +56,3 @@ wof.init.step0 = function() {
 
 };
 
-wof.loadAspectsFromButton = function() {
-    wof.loadAspects(wof.numberToGenerate, wof.chaosLevel, wof.longFactor);
-};
-
-wof.loadAspects = function(numberToLoad, chaosLevel, longFactor) {
-    var salida = $("#aspectList");
-
-    for (var i = 0; i < numberToLoad; i++) {
-        salida.append($("<div></div>").addClass("aspect").addClass(wof.fuente.actual).append(wof.aspects.random.any(chaosLevel, longFactor)));
-    }
-};
-
-
-wof.clearAspects = function() {
-    $("#aspectList").empty();
-};
-
-
-wof.changeLongFactor = function(valor) {
-    wof.longFactor = valor/100;
-};
-
-wof.changeNumberToGenerate = function(valor) {
-    valor = Math.floor(valor);
-    wof.numberToGenerate = valor;
-    if (valor > 1)
-        $("#botonGenerar").html("Generate other " + valor + "!")
-    else
-        $("#botonGenerar").html("Generate just one!")
-};
-
-wof.changeChaosLevel = function(valor) {
-    wof.chaosLevel = valor/100;
-};
--- js/wof/wof.js
+++ js/wof/wof.js
@@ -11,3 +11,38 @@ wof.l = jsrpg.locale.l;
 wof.numberToGenerate = 10;
 wof.longFactor = 0.5;
 wof.chaosLevel = 0.5;
+
+wof.loadAspectsFromButton = function() {
+    wof.loadAspects(wof.numberToGenerate, wof.chaosLevel, wof.longFactor);
+};
+
+wof.loadAspects = function(numberToLoad, chaosLevel, longFactor) {
+    var salida = $("#aspectList");
+
+    for (var i = 0; i < numberToLoad; i++) {
+        salida.append($("<div></div>").addClass("aspect").addClass(wof.fuente.actual).append(wof.aspects.random.any(chaosLevel, longFactor)));
+    }
+};
+
+
+wof.clearAspects = function() {
+    $("#aspectList").empty();
+};
+
+
+wof.changeLongFactor = function(valor) {
+    wof.longFactor = valor/100;
+};
+
+wof.changeNumberToGenerate = function(valor) {
+    valor = Math.floor(valor);
+    wof.numberToGenerate = valor;
+    if (valor > 1)
+        $("#botonGenerar").html("Generate other " + valor + "!")
+    else
+        $("#botonGenerar").html("Generate just one!")
+};
+
+wof.changeChaosLevel = function(valor) {
+    wof.chaosLevel = valor/100;
+};
